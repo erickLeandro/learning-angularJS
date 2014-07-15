78e6e1bf610941c79b6f7c6cdd548d8284df729e
Erick Leandro <web5@trinix.com.br>
2014-07-15 13:38:03 -0400

    Level 4 complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
+'use strict';
 var app = angular.module('store', []);
+
+app.directive('productTitle', function(){
+
+	return {
+	
+		restrict: 'E',
+		templateUrl: 'product-title.html'
+	
+	};
+
+});
+
+app.controller('StoreController', ['$scope', function($scope){
+
+	var products = [
+	{
+	
+		name: 'Mouse and Keyboard Wireless',
+		price: '150,00',
+		description: 'Nice product',
+		images: [],
+		reviews: [
+		{
+			stars: 5,
+			body: 'I love this product',
+			author: 'Cristiano ronaldo',
+		},
+		{
+			stars: 1,
+			body: 'This product sucks',
+			author: 'Lionel Messi',
+		}
+		]
+	}
+	];
+	
+	$scope.products = products;
+
+}]);
